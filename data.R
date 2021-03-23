80f3e95dc72fd4c279306549f5dd9319d962eb51
sccmckenzie <sccmckenzie@gmail.com>
2021-03-23 11:26:02 -0500

    change customer_df to hms

--- data.R
+++ data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(lubridate)
+library(hms)
 
 # Dataset 1
 
@@ -22,18 +23,18 @@ event_df %>%
 
 # Dataset 2
 
-t01 <- as_datetime("2020-02-03 08:00:00")
-t02 <- as_datetime("2020-02-03 19:00:00")
+t01 <- as_hms("08:00:00")
+t02 <- as_hms("19:00:00")
+n <- 10
 
 set.seed(27)
 
-n <- 10
-
 customer_df <- tibble(customer = 1:n,
-       enter = runif(n, t01, t02) %>% as_datetime(),
-       exit = enter + rbeta(n, 2, 2.5) * dhours(2),
+       enter = runif(n, t01, t02) %>% as_hms(),
+       exit = (enter + rbeta(n, 2, 2.5) * 7200) %>% as_hms(),
        revenue = time_length(exit - enter, unit = "hours") * 30 + rnorm(n, mean = 0, sd = 15)) %>%
-  mutate(revenue = if_else(revenue < 0, 0, revenue)) %>%
+  mutate(revenue = if_else(revenue < 0, 0, revenue),
+         across(where(is_hms), round_hms, 60)) %>%
   relocate(revenue, .before = enter)
 
 # customer_df %>%
